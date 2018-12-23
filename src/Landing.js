7731683bf6de79cd71f47ce7fd532d3942deb762
Skyler Riley <riley.skyler@gmail.com>
2018-12-23 13:38:14 -0700

    initial commit

---
+++ src/Landing.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+import profile_photo from '../src/profile_photo.jpg'
+
+export class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="jumbotron">
+
+            <div className="name">Skyler Riley</div>
+
+            <div className="photo">
+                <img src={profile_photo} alt="Ss"/>
+            </div>
+
+            <div className="details">
+                <div className="detail">Web</div>                   
+                <div className="barrier"></div>
+                <div className="detail">Blockchain</div>
+            </div>
+    
+            <div className="name">Developer</div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
